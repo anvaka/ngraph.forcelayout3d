0d4cad83fda2532aac77f46e5477dad4cc8b0144
anvaka <anvaka@gmail.com>
2015-01-25 02:06:55 -0800

    Fixed movement calculation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * This module provides all required forces to regular ngraph.physics.simulator
- * to make it 3d simulator. Ideally ngraph.phoysics.simulator should operate
+ * to make it 3D simulator. Ideally ngraph.physics.simulator should operate
  * with vectors, but on practices that showed performance decrease... Maybe
  * I was doing it wrong, will see if I can refactor/throw away this module.
  */
@@ -10,7 +10,7 @@ function createLayout(graph, physicsSettings) {
   var createSimulator = require('ngraph.physics.simulator');
   var createForceLayout = require('ngraph.forcelayout');
   var merge = require('ngraph.merge');
-  var physicsSettings = merge(physicsSettings, {
+  physicsSettings = merge(physicsSettings, {
         createQuadTree: require('ngraph.quadtreebh3d'),
         createBounds: require('./lib/bounds'),
         createDragForce: require('./lib/dragForce'),
