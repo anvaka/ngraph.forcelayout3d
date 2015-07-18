6fd056ac369dd386565efdce2410d6d70113e731
anvaka <anvaka@gmail.com>
2015-07-18 11:51:32 -0700

    Added perf test for fixed nodes

--- perf/index.js
+++ perf/index.js
@@ -1,13 +1,29 @@
-var graph = require('ngraph.generators').grid(20, 20);
+var create = require('ngraph.generators');
 
 var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
 
 // add tests
 suite.add('Run default', function() {
+  var graph = create.grid(20, 20);
   var layout = require('../')(graph);
   for (var i = 0; i < 20; ++i) {
     layout.step();
+  }
+}).add('Mostly fixed nodes', function() {
+  var graph = create.grid(20, 20);
+  var layout = require('../')(graph);
+  graph.forEachNode(fixIt);
+  // add tiny amount of unfixed nodes:
+  graph.addLink(0, -1);
+  graph.addLink(0, -2);
+
+  for (var i = 0; i < 20; ++i) {
+    layout.step();
+  }
+
+  function fixIt(node) {
+    layout.pinNode(node, true);
   }
 })
 .on('cycle', function(event) {
